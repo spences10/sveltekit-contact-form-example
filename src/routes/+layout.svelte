8e7c655fe5d05291c91aae7a0bb89530cf3d920d
spences10 <spences10apps@gmail.com>
2025-03-08 07:53:58 +0000

    refactor: :recycle: tw v4 and daisyui v5 migration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
-<script>
-	import '../app.postcss';
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
 </script>
 
 <main
 	class="container mx-auto max-w-3xl prose prose-xl prose-a:link-secondary prose-h1:text-secondary prose-h2:text-secondary"
 >
-	<slot />
+	{@render children()}
 </main>
